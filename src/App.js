9b13429215a846485900b428a2ff5535f4a1f9a6
narimani <narimani@namatech.co.uk>
2023-04-16 20:27:40 +0430

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3> Manage contacts</h3>
+
+      {/* just for test if font awesome and bootstrap will work */}
+      <button className='btn btn-primary'>button</button>
+      <span className='fa fa-user'></span>
+    </div>
+  );
+}
+
+export default App;
