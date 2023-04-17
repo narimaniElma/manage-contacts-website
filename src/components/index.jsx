95806ea2d3be6553e6566fd88a21bfd75d1581c5
narimani <narimani@namatech.co.uk>
2023-04-17 16:03:50 +0430

    Better way for importing

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+// Better way for importing
+
+// Contact Component
+export {default as Contact} from '../components/Contacts/Contact';
+export {default as AddContact} from '../components/Contacts/AddContact';
+export {default as EditContact} from '../components/Contacts/EditContact';
+export {default as ViewContact} from '../components/Contacts/ViewContact';
+export {default as Contacts} from '../components/Contacts/Contacts';
+export {default as ContactSearch} from '../components/Contacts/ContactSearch';
+
+// Other component
+export {default as Navbar} from '../components/Navbar';
+export {default as Spinner} from '../components/Spinner';
