ce7b7eded9533c07fd9d96fb06cd1c2b4b2f5565
narimani <narimani@namatech.co.uk>
2023-04-21 15:56:28 +0430

    create json-server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import {Navbar, Contacts, AddContact, EditContact, ViewContact} from "./components";
+import {
+  Navbar,
+  Contacts,
+  AddContact,
+  EditContact,
+  ViewContact,
+} from "./components";
 
 import "./App.css";
 
@@ -11,7 +18,16 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Contacts getContacts={contacts} loading={loading}/>
+      <Routes>
+        <Route path="/" element={<Navigate to="/contacts" />} />
+        <Route
+          path="/contacts"
+          element={<Contacts getContacts={contacts} loading={loading} />}
+        />
+        <Route path="/contacts/add" element={<AddContact />} />
+        <Route path="/contacts/:id" element={<ViewContact />} />
+        <Route path="/contacts/edit/:id" element={<EditContact />} />
+      </Routes>
     </div>
   );
 }
